04d3f6b9294f0ab48542acd7391244394a8943ec
sakurai <sakurai_ch@yahoo.co.jp>
2021-02-11 22:19:01 +0900

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,48 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
 import Home from '../views/Home.vue'
+import Profile from '../views/Profile.vue'
+import Detail from '../views/Detail.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  },
+  {
+    path: '/detail/:id',
+    name: 'Detail',
+    component: Detail,
+    props: true
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
